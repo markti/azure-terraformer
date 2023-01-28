7e90122d45010fc1ead28c02363ceebaf1481f2b
markti <markti@outlook.com>
2023-01-27 23:44:27 -0500

    add ep1

---
+++ episodes/001/main.tf
@@ -0,0 +1,26 @@
+resource random_string main {
+  length           = 8
+  upper            = false
+  special          = false
+}
+
+resource azurerm_resource_group main {
+  name     = "rg-ep1-${random_string.main.result}"
+  location = var.location
+}
+
+resource azurerm_storage_account main {
+  name                     = "st${random_string.main.result}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource azurerm_log_analytics_workspace main {
+  name                = "log-ep1-${random_string.main.result}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
