02688b9d516f1c8c189f578f0cb2196afaf1407d
markti <markti@outlook.com>
2023-01-31 09:12:56 -0500

    ep7

---
+++ episodes/007/app/main.tf
@@ -0,0 +1,14 @@
+locals {
+  name = "ep7"
+}
+
+resource random_string main {
+  length           = 8
+  upper            = false
+  special          = false
+}
+
+resource azurerm_resource_group main {
+  name     = "rg-${local.name}-${random_string.main.result}"
+  location = var.location
+}
