c056f4d8e5c799043f66de8800b424896d102fb7
markti <markti@outlook.com>
2023-02-28 21:48:04 -0500

    fix

--- episodes/015/terraform/minecraft-tester/vm.tf
+++ episodes/015/terraform/minecraft-tester/vm.tf
@@ -4,7 +4,7 @@ data azurerm_shared_image_version minecraft_local {
   gallery_name        = var.gallery_name
   resource_group_name = var.gallery_resource_group
 }
-/*
+
 module bedrock_local {
 
   source = "../modules/vm/linux-public"
