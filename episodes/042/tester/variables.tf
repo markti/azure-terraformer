5c476a88396fafd16671e8fcfbfda21b21f6cca0
markti <markti@outlook.com>
2023-04-30 17:15:07 -0400

    ep42

---
+++ episodes/042/tester/variables.tf
@@ -0,0 +1,30 @@
+variable "application_name" {
+  type = string
+}
+variable "environments" {
+  type = list(string)
+}
+variable "azure_credentials" {
+  type = map(
+    object(
+      {
+        client_id       = string
+        client_secret   = string
+        tenant_id       = string
+        subscription_id = string
+      }
+    )
+  )
+}
+variable "azure_backends" {
+  type = map(
+    object({
+      resource_group  = string
+      storage_account = string
+      container       = string
+    })
+  )
+}
+variable "repository_template_url" {
+  type = string
+}
